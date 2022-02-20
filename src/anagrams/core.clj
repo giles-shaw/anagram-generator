6e6df5d35518eb8723ed46907e9835351edff703
Giles <giles.shaw@gmail.com>
2022-02-20 13:03:13 +0000

    Documentation, replace accumulator/remainder vecs with maps

--- src/anagrams/core.clj
+++ src/anagrams/core.clj
@@ -18,9 +18,13 @@
        (string/split
          (slurp (io/resource "corncob_lowercase.txt")) #"\n")))
 
-(defn is-in [char-freqs-one char-freqs-two]
-  (every? true? (map (fn [[k v]](>= (get char-freqs-one k 0) v))
-                       char-freqs-two)))
+(defn constructible? [char-freqs word]
+    (->> word
+         frequencies
+         (map (fn [[ch ct]] (>= (get char-freqs ch 0) ct)))
+         (every? true?))
+  ; (every? true? (map (fn [[k v]](>= (get char-freqs k 0) v)) (frequencies word)))
+  )
 
 (defn subtract-word [char-freqs word]
   (into {} (filter (fn [[_ v]] (> v 0)))
@@ -28,7 +32,7 @@
 
 (defn filter-wordlist
   [allowed-chars wordlist]
-  (filter #(is-in allowed-chars (frequencies %)) wordlist))
+  (filter (partial constructible? allowed-chars) wordlist))
 
 
 ;;
@@ -47,7 +51,10 @@
 ;anagrams
 ;;
 
-(defn contained-words [prefix phrase-chars dictionary]
+(defn contained-words 
+  "Returns the set of words in `dictionary` beginning with `prefix` which can
+  be constructed from `phrase-chars`."
+  [prefix phrase-chars dictionary]
   (let [phrase-chars    (->> phrase-chars
                              (filter (fn [[_ ct]] (> ct 0)))
                              (into {}))
@@ -61,62 +68,67 @@
                                     options))]
     (if (:word prefixed-dict) (conj successor-words prefix) successor-words)))
 
-
-(defn next-word-transfers [[accumulator remainder] dictionary]
-  (if (empty? remainder) [[accumulator remainder]]
+(defn next-word-transfers
+  "Returns a sequence of all `{:keys [accumulator remainder]}` maps that result from
+  transferring a valid word from `remainder` to `accumulator`."
+  [{:keys [accumulator remainder] :as pair} dictionary]
+  (if (empty? remainder) [pair]
     (let [words        (contained-words "" remainder dictionary)
           remainders   (map (partial subtract-word remainder) words)
           accumulators (map (fn [w] (update accumulator w #(inc (or % 0)))) words)]
-      (map vector accumulators remainders))))
+      (map #(zipmap [:accumulator :remainder] [%1 %2]) accumulators remainders))))
 
 (defn update-accumulators 
-  "Returns the set of all (accumulator, remainder) pairs that can result from
-  transferring a word from each remainder in `accumulator-remainder-pairs` to the
-  corresponding accumulator. Pairs in `accumulator-remainder-pairs` whose remainder is
-  nonempty but does not contain any words in `dictionary` are removed, and pairs whose
-  remainder is empty are carried forward without change."
+  "Returns the set of all `{:keys [accumulator remainder]}` maps that can
+  result from transferring a word from a `remainder` value in
+  `accumulator-remainder-pairs` to the corresponding `accumulator` value. Maps
+  in `accumulator-remainder-pairs` whose `remainder` is nonempty but does not
+  contain any words in `dictionary` are removed. Pairs whose `remainder` is
+  empty are carried forward unchanged."
   [accumulator-remainder-pairs dictionary]
   (let [updates (map #(next-word-transfers % dictionary)
                      accumulator-remainder-pairs)] 
-  (reduce into #{} updates)))  ; remove repetitions
+  (reduce into #{} updates)))  ; deduplicate
 
-(defn anagram-steps
-  [pairs dictionary]
-  (let [accumulations (map first pairs)
-        remainders    (map second pairs)]
-  (if (every? empty? remainders) accumulations
-    (recur (update-accumulators pairs dictionary) dictionary))))
+(defn anagrams
+  "Returns the sequence of all frequency maps whose keys are words from
+  `dictionary` which can be be constructed using characters from `phrase-chars`
+  without replacement,"
+  [phrase-chars dictionary]
+  (let [init-pairs #{{:accumulator {} :remainder phrase-chars}}
+        steps      (iterate #(update-accumulators % dictionary) init-pairs)
+        remainder? (fn [acc-remainder-pairs]
+                     (some seq (map :remainder acc-remainder-pairs)))]
+    (map :accumulator (first (drop-while remainder? steps)))))
 
-; (defn anagram-steps [pairs dictionary]
-;   (let [steps (iterate #(update-accumulators % dictionary) pairs)
-;         some-non-empty-remainder? (fn [acc-remainder-pairs]
-;                                     (some? (map (comp seq :remainder)
-;                                                 acc-remainder-pairs)))]
-;     (map :accumulator
-;          (first (drop-while some-non-empty-remainder? steps)))))
 
-(defn render [word-dict]
+;;
+; display and main
+;;
+
+(defn render-anagram [word-dict]
   (let [repetitions (map #(apply repeat (reverse %)) word-dict)]
     (->> repetitions flatten sort (interpose " ") (apply str))))
 
-(defn anagrams [phrase-chars dictionary]
-  (sort (map render (anagram-steps #{[{} phrase-chars]} dictionary))))
+(defn render [anagram-list]
+  (->> anagram-list (map render-anagram) sort (interpose "\n") (apply str)))
 
 (defn main
   [phrase]
   (let [phrase-chars (-> phrase preprocess frequencies)
         dictionary   (->> (load-dict) (filter-wordlist phrase-chars) make-trie)
-        anagrams (anagrams phrase-chars dictionary)]
-   (apply str (interpose "\n" anagrams))))
+        results      (anagrams phrase-chars dictionary)]
+   (render results)))
+
 
 ;;
 ;test examples
 ;;
-; (def test-phrase "parmesan")
+(def test-phrase "parmesan")
 ; (def phrase-chars (frequencies (preprocess test-phrase)))
 ; (def filtered-dict (make-trie (filter-wordlist phrase-chars (load-dict))))
 ; (count (anagrams phrase-chars filtered-dict))
 ; (first (anagrams phrase-chars filtered-dict))
 
 ; (time (main test-phrase))
-; (println (main test-phrase))
+(println (main test-phrase))
